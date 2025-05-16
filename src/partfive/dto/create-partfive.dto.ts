12c27dd7b0ac729ebb939752183e3ed927a72eb9
LePhanHaiLong <longlph.23it@gmail.com>
2025-05-17 00:47:18 +0700

    create schema part five

--- src/partfive/dto/create-partfive.dto.ts
+++ src/partfive/dto/create-partfive.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePartfiveDto {}
+import { IsNotEmpty, IsOptional } from "class-validator";
+
+export class CreatePartfiveDto {
+    @IsNotEmpty({ message: 'Tên bộ đề không được để trống' })
+    name: string;
+
+    @IsNotEmpty({ message: 'Miêu tả không được để trống' })
+    description: string;
+
+    @IsOptional()
+    audioUrl: string;
+}
--- src/partfive/partfive.module.ts
+++ src/partfive/partfive.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PartfiveService } from './partfive.service';
 import { PartfiveController } from './partfive.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PartFive, PartFiveSchema } from './schema/partfive.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: PartFive.name, schema: PartFiveSchema }])],
   controllers: [PartfiveController],
   providers: [PartfiveService],
 })
-export class PartfiveModule {}
+export class PartfiveModule { }
