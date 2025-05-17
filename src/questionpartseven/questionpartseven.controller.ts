01818f4342e369bd53d8a4688c7c81c00db25b01
LePhanHaiLong <longlph.23it@gmail.com>
2025-05-17 23:19:10 +0700

    upload question part 7

--- src/questionpartseven/questionpartseven.controller.ts
+++ src/questionpartseven/questionpartseven.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { QuestionpartsevenService } from './questionpartseven.service';
 import { CreateQuestionpartsevenDto } from './dto/create-questionpartseven.dto';
 import { UpdateQuestionpartsevenDto } from './dto/update-questionpartseven.dto';
+import { ResponseMessage } from 'src/decorator/customize.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('questionpartseven')
 export class QuestionpartsevenController {
-  constructor(private readonly questionpartsevenService: QuestionpartsevenService) {}
+  constructor(private readonly questionpartsevenService: QuestionpartsevenService) { }
 
   @Post()
-  create(@Body() createQuestionpartsevenDto: CreateQuestionpartsevenDto) {
-    return this.questionpartsevenService.create(createQuestionpartsevenDto);
+  @ResponseMessage("Tạo mới câu hỏi thành công")
+  @UseInterceptors(FileInterceptor('imageUrl'))
+  create(@Body() createQuestionpartsevenDto: CreateQuestionpartsevenDto, @UploadedFile() img: Express.Multer.File) {
+    return this.questionpartsevenService.create(createQuestionpartsevenDto, img);
   }
 
   @Get()
--- src/questionpartseven/questionpartseven.service.ts
+++ src/questionpartseven/questionpartseven.service.ts
@@ -1,11 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateQuestionpartsevenDto } from './dto/create-questionpartseven.dto';
 import { UpdateQuestionpartsevenDto } from './dto/update-questionpartseven.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { QuestionPartSeven } from './schema/questionpartseven.schema';
+import { Model } from 'mongoose';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
 export class QuestionpartsevenService {
-  create(createQuestionpartsevenDto: CreateQuestionpartsevenDto) {
-    return 'This action adds a new questionpartseven';
+  constructor(
+    @InjectModel(QuestionPartSeven.name)
+    private questionModal: Model<QuestionPartSeven>,
+    private cloudinary: CloudinaryService,
+  ) { }
+  async uploadImageToCloudinary(file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('No file provided.');
+    }
+    return await this.cloudinary.uploadImage(file).catch(() => {
+      throw new BadRequestException('Invalid file type.');
+    });
+  }
+  async create(createQuestionpartsevenDto: CreateQuestionpartsevenDto, img: Express.Multer.File) {
+    let imageUrl = null;
+    imageUrl = await this.uploadImageToCloudinary(img);
+    return await this.questionModal.create({ ...createQuestionpartsevenDto, imageUrl: imageUrl?.secure_url });
   }
 
   findAll() {
